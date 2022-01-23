3f9bba229bf69035a93e8023b6da065b688a3cc1
DevSunshine01 <53397088+DevSunshine01@users.noreply.github.com>
2022-01-22 22:14:19 -0800

    DirScanner recursion method implemented
    Currently undergoing modification to address an issue with filetype 'file'. These aren't folders and do not include a '.' commonly in files.

--- directoryScraper.py
+++ directoryScraper.py
@@ -1,10 +1,12 @@
 import os
 from xml.dom import minidom
 import time
-
+import filetype
 path = 'C:\\'
 DirectoryScraper = 'C:\\DirectoryScraper\\'
 outputFileExt = '.txt'
+kind = filetype.guess('C:\\Microsoft\\AndroidNDK64\\android-ndk-r16b\\build\\gmsl\\gmsl\\')
+print(kind)
 ##this can be used to set your own directory to scan manually
 ##path = 'C:\\'
 
@@ -14,53 +16,38 @@ outputFileExt = '.txt'
 ##writer = open((DirectoryScraper+'data'+outputFileExt),"w")
 
 
-def searchDir(path):
+def searchDir(newPath):
+    path = newPath
+    print("\n"+path + "\\")
+    print("-----------------------------")
     for filename in os.listdir(path):
-        ##if not len(os.listdir(path)) >1: continue
+        ##if the directory doesn't have one item in it at least, skip this code
+        if not len(os.listdir(path)) >=1: continue
+        ##my current file is this directory plus the filename in this loop
         currentDir = os.path.join(path, filename)
-        print('\n\n'+currentDir)
-        print("-----------------------------")        
-        if not '.' in currentDir:
-            if filename == 'System Volume Information': continue ##these two directories run into access issues, skip over
+       
+        if not '.' in filename: ##look for folders without a '.' in the file extension
+            ##these directories run into access issues, skip over
+            if filename == 'System Volume Information': continue 
             elif filename == 'Documents and Settings': continue
-            path = os.path.join(path, filename)        
+            elif filename == 'Windows': continue
+            
+            ##the new path is this path plus the next folder
+            nextPath = os.path.join(path, filename)
             try:
-                for filename in os.listdir(path):
-                    nextFile = os.path.join(path, filename)
-                    print(nextFile)
-                path = os.path.dirname(path)
-            except:
-                print('\nError reading '+ path)            
+                searchDir(nextPath)
+            except FileNotFoundError:
+                continue;     
         else:
             print(filename)
             
 ##while the count of files in a folder > 1 and no dot in the name:
 searchDir(path)
 
-##    
-##for filename in os.listdir(path):
-##    currentDir = os.path.join(path, filename)
-##    print('\n\n'+currentDir)
-##    print("-----------------------------")
-##    if not '.' in currentDir:
-##        if filename == 'System Volume Information': continue ##these two directories run into access issues, skip over
-##        elif filename == 'Documents and Settings': continue
-##        path = os.path.join(path, filename)        
-##        try:
-##            for filename in os.listdir(path):
-##                nextFile = os.path.join(path, filename)
-##                print(nextFile)
-##            path = os.path.dirname(path)
-##        except:
-##            print('\nError reading '+ path)            
-##    else:
-##        print(filename)  
-        
-
-    
-   
-    
-
 ##writeFile.write(elem.firstChild.data + ",/n")       
 ##print(str(processed) + ' Files processed')        
 ##writer.close()
+
+
+####C:\Python> python.exe "C:\Python\Scripts\DirScraper\DirectoryScanner\directoryScraper.py"
+
