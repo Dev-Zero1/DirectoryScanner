0f71c0f66e579810a49837c7ac7e0ccfded5d5d6
DevSunshine01 <53397088+DevSunshine01@users.noreply.github.com>
2022-01-24 02:18:14 -0800

    clarified database select statement's output
    did a base-level clarification of the row outputs, optimization is later.

--- dbFetch.py
+++ dbFetch.py
@@ -2,7 +2,8 @@ import File
 
 def setCredentials():
   credFile = 'C:\\Python\\Scripts\\sqlData\\sqlInfo.csv'
-
+  ##this file has my username and password separated by comma
+  ##aids in keeping my credentials for the database off the github repo
   with open(credFile, mode ='r')as myFile:
     lines = myFile.readlines()
     lines = lines[0].split(",")
@@ -37,11 +38,11 @@ def fetch():
   result = mc.fetchall()
 
   for row in result:
-      print(row[1])
+      print(row[1]) ##fileName
       print("---------------------")
-      print(row[2])
-      print(row[3])
-      print(row[4])
-      print(str(row[5])+'\n')
+      print(row[2])##fileDir
+      print(row[3])##fileType
+      print(row[4])##fileLastModified
+      print(str(row[5])+'\n')##fileSize
 
 
